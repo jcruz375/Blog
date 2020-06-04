389e3db3977369803e7f27cb35927018993b81ac
joao cruz <joaocruz375@gmail.com>
2020-06-04 20:14:37 -0300

    V1.0

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 const sequelize = require('sequelize');
-const connection = new sequelize('bancoblog', 'j.cruz01', 'U.LiAS6DqSxM#sm', {
+const connection = new sequelize('blogsql', 'j.cruz00', 'U.LiAS6DqSxM#sm', {
     host: 'mysql669.umbler.com',
     dialect: 'mysql',
     timezone: '-03:00'
